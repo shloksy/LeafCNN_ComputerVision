e3065ed1bb017b34db87a1c57659a1aa6362ab97
shloksy <162043636+shloksy@users.noreply.github.com>
2025-05-03 15:32:33 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ st.set_page_config(page_title="LeafGuard", layout="centered")
 from PIL import Image
 import torch
 from torchvision import transforms
+import torch.nn.functional as F
 
 from model import LeafDiseaseClassifier
 
-@st.cache(allow_output_mutation=True)
+@st.cache_data(allow_output_mutation=True)
 def load_model(checkpoint_path: str):
     model = LeafDiseaseClassifier()
     ckpt   = torch.load(checkpoint_path, map_location="cpu")
@@ -30,6 +31,12 @@ preprocess = transforms.Compose([
 st.title("🌿 LeafGuard")
 st.write("Upload a tomato leaf image and get a disease prediction.")
 
+class_names = ['Bacterial_Spot', 'Early_Blight', 
+               'Healthy', 'Late_Blight', 'Leaf_Mold', 
+               'Mosaic_Virus', 'Septoria_Leaf_Spot', 
+               'Target_Spot', 'Two_Spotted_Spider_Mite', 
+               'Yellowleaf__Curl_Virus']
+
 uploaded = st.file_uploader("Choose an image file", type=["jpg", "png"])
 if uploaded:
     img = Image.open(uploaded).convert("RGB")
@@ -38,6 +45,9 @@ if uploaded:
     tensor = preprocess(img).unsqueeze(0)
     with torch.no_grad():
         output = model(tensor)
-        prediction = output.argmax(dim=1).item()
+        prediction = F.softmax(output,dim=1).item()
+        conf, idx = prediction.max(dim=1)
+        pred_label = class_names[idx.item()]
+        conf_pct   = conf.item() * 100
 
-    st.success(f"**Prediction:** {prediction}")
+    st.success(f"**Prediction:** {prediction} \n**Confidence:** {conf_pct:.2f}%")
