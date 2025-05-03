d9ba8218d0ba63ebdc2a9533543b2c1721ad17ff
shloksy <162043636+shloksy@users.noreply.github.com>
2025-05-03 15:13:27 -0400

    Create model.py

---
+++ model.py
@@ -0,0 +1,39 @@
+class Network(nn.Module):
+    def __init__(self):
+        super(Network, self).__init__()
+
+        self.conv_layers = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            #nn.BatchNorm2d(32),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            #nn.BatchNorm2d(64),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            #nn.BatchNorm2d(128),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+
+        self.linear_layers = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(128 * 16 * 16, 512),
+            nn.ReLU(),
+            nn.Dropout(0.4),
+
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+
+            nn.Linear(256, 10)
+        )
+
+    def forward(self, x):
+        # Define program flow
+        x = self.conv_layers(x)
+        x = self.linear_layers(x)
+        return x
